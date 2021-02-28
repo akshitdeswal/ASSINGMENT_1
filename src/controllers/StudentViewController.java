bc355ea8476c82c45ed9c0b8cad160fb4493dde0
akshit-deswal <72546148+akshit-deswal@users.noreply.github.com>
2021-02-27 23:26:01 -0500

    Add the different view objects and controlled them.

--- src/controllers/StudentViewController.java
+++ src/controllers/StudentViewController.java
@@ -1,5 +1,41 @@
 package controllers;
 
-public class StudentViewController {
-    
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import models.Student;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
+public class StudentViewController implements Initializable {
+    @FXML
+    private Label firstNameLabel;
+
+    @FXML
+    private Label lastNameLabel;
+
+    @FXML
+    private Label studentNumberLabel;
+
+    @FXML
+    private ListView interestList;
+
+    private Student student;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ArrayList<String> unchartedInterests = new ArrayList<>();
+        unchartedInterests.addAll(Arrays.asList("dancing","sports","music"));
+        student = new Student("AKSHIT","DESWAL", 200454399, unchartedInterests);
+        firstNameLabel.setText(student.getFirstName());
+        lastNameLabel.setText(student.getLastName());
+        studentNumberLabel.setText(Integer.toString(student.getStudentNumber()));
+//
+
+
+    }
 }
