543acab40da75bbf0232903abb9c169a7e3e0558
akshit-deswal <72546148+akshit-deswal@users.noreply.github.com>
2021-02-27 23:23:56 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("views/studentView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Viewing a Single Student Object");
+        stage.show();
+    }
+}
---
+++ src/models/Student.java
@@ -0,0 +1,82 @@
+package models;
+import java.util.ArrayList;
+
+
+public class Student {
+    //    three private variables.
+    private String firstName;
+    private String lastName;
+    private int studentNumber;
+    private ArrayList<String> interests;
+
+    /*
+    The Constructor takes 4 argument two of String type, one integer type and one is ArrayList<String> type.
+     */
+    public Student(String firstName, String lastName, int studentNumber, ArrayList<String> interest){
+        setFirstName(firstName);
+        setLastName(lastName);
+        setStudentNumber(studentNumber);
+        setInterest(interest);
+    }
+
+    //    setter and getter for each variable.
+    public void setFirstName(String firstName){
+        if (firstName.length() >= 2)
+            this.firstName = firstName;
+        else
+            throw new IllegalArgumentException(firstName+" is invalid please your First name should contain atleast 2 character ");
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setLastName(String lastName){
+        if(lastName.length() >= 2)
+            this.lastName = lastName;
+        else
+            throw new IllegalArgumentException(lastName+" is invalid please your Last name should contain atleast 2 character");
+    }
+    public  String getLastName(){
+        return lastName;
+    }
+    public void setStudentNumber(int studentNumber){
+        if(studentNumber>100000000&&studentNumber<999999999)
+            this.studentNumber = studentNumber;
+        else
+            throw new IllegalArgumentException(studentNumber+" is not valid, please type a correct nine digit number");
+    }
+    public int getStudentNumber(){
+        return studentNumber;
+    }
+
+    /*
+    Static method which returns an ArrayList of type String and returns the same type of data.
+     */
+    public static ArrayList<String> availableInterest()
+    {
+        ArrayList<String> interests = new ArrayList<>();//new ArrayList or we can use List here
+        interests.add("hiking");//all the valid inputs
+        interests.add("skiing");
+        interests.add("reading");
+        interests.add("coding");
+        interests.add("gaming");
+        interests.add("music");
+        interests.add("dancing");
+        interests.add("sports");
+        return interests;
+    }
+
+    public void setInterest(ArrayList<String> interests) {
+        ArrayList<String> validInterest = availableInterest();
+        for (String interest : interests)//for loop is used to iterate the ArrayList.
+        {
+            if (!validInterest.contains(interest))//condition
+                throw new IllegalArgumentException(interest + " is not valid.  please choose from these options: " +validInterest);
+        }
+        this.interests = interests;
+    }
+    public ArrayList<String> getInterest(){
+        return interests;
+    }
+
+
+}
